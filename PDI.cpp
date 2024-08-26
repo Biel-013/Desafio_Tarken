3ab59457ff2e9ae197bb0c730d60a804c183c987
Biel-013 <luizgomesgabrielmg@gmail.com>
2024-08-25 22:19:44 -0300

    Contagem teminada dos meteroros que caem na água, como imagem que ilustra o caminho dos meteoros

--- PDI.cpp
+++ PDI.cpp
@@ -13,37 +13,76 @@ int main(int argc, char **argv)
         std::cerr << "Erro ao carregar a imagem!" << std::endl;
         return -1;
     }
-
+    /************************Clean image**************************/
     cv::Vec3b pixelWhite(255, 255, 255);
-    cv::Vec3b pixelRed(0, 0, 255);
-
-    for (int i = 0; i < image.rows; i++)
+    for (int y = 0; y < image.rows; y++)
     {
-        for (int j = 0; j < image.cols; j++)
+        for (int x = 0; x < image.cols; x++)
         {
-            
-            if (image.at<cv::Vec3b>(i, j)[0] == pixelWhite[0] && // Componente B
-                image.at<cv::Vec3b>(i, j)[1] == pixelWhite[1] && // Componente G
-                image.at<cv::Vec3b>(i, j)[2] == pixelWhite[2])
-            { 
-
-                std::cout << "Estrela encontrado em (" << i << ", " << j << ")" << std::endl;
-                image.at<cv::Vec3b>(i, j) = cv::Vec3b(0, 255, 0); // B=255, G=0, R=0
-                Star_cont++;
-            }
-
-            if (image.at<cv::Vec3b>(i, j)[0] == pixelRed[0] &&
-                image.at<cv::Vec3b>(i, j)[1] == pixelRed[1] && 
-                image.at<cv::Vec3b>(i, j)[2] == pixelRed[2])
+            if (image.at<cv::Vec3b>(y, x)[0] == pixelWhite[0] &&
+                image.at<cv::Vec3b>(y, x)[1] == pixelWhite[1] &&
+                image.at<cv::Vec3b>(y, x)[2] == pixelWhite[2])
             {
-
-                std::cout << "Meteoro encontrado em (" << i << ", " << j << ")" << std::endl;
-                Meteor_cont++;
+                image.at<cv::Vec3b>(y, x) = cv::Vec3b(189, 119, 2);
             }
         }
     }
-    std::cout << "O numero de meteoros e: " << Meteor_cont << "\nE o numero de estrelas e: " << Star_cont << std::endl;
-    // cv::imwrite("imagem_alterada.png", image);
+    cv::imwrite("Clean_image.png", image);
+    /************************Clean image**************************/
+    cv::Vec3b pixelRed(0, 0, 255);
+    cv::Vec3b pixelBlue(255, 0, 0);
+    cv::Vec3b pixelYellow(0, 255, 255);
+
+    for (int y = 0; y < image.rows; y++)
+    {
+        for (int x = 0; x < image.cols; x++)
+        {
+            if (image.at<cv::Vec3b>(y, x)[0] == pixelRed[0] &&
+                image.at<cv::Vec3b>(y, x)[1] == pixelRed[1] &&
+                image.at<cv::Vec3b>(y, x)[2] == pixelRed[2])
+            {
+                for (int i = y; i < image.rows; i++)
+                {
+                    image.at<cv::Vec3b>(y, x) = cv::Vec3b(255, 255, 255);
+                    if (image.at<cv::Vec3b>(i, x)[0] == pixelBlue[0] &&
+                        image.at<cv::Vec3b>(i, x)[1] == pixelBlue[1] &&
+                        image.at<cv::Vec3b>(i, x)[2] == pixelBlue[2])
+                    {
+                        Meteor_cont++;
+                        image.at<cv::Vec3b>(y, x) = cv::Vec3b(0, 255, 255);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    for (int y = 0; y < image.rows; y++)
+    {
+        for (int x = 0; x < image.cols; x++)
+        {
+            if (image.at<cv::Vec3b>(y, x)[0] == pixelWhite[0] &&
+                image.at<cv::Vec3b>(y, x)[1] == pixelWhite[1] &&
+                image.at<cv::Vec3b>(y, x)[2] == pixelWhite[2])
+            {
+                for (int i = y; i < image.rows; i++)
+                {
+                    image.at<cv::Vec3b>(i, x) = cv::Vec3b(0, 0, 255);
+                }
+            }
+            if (image.at<cv::Vec3b>(y, x)[0] == pixelYellow[0] &&
+                image.at<cv::Vec3b>(y, x)[1] == pixelYellow[1] &&
+                image.at<cv::Vec3b>(y, x)[2] == pixelYellow[2])
+            {
+                for (int i = y; i < image.rows; i++)
+                {
+                    image.at<cv::Vec3b>(i, x) = cv::Vec3b(0, 255, 0);
+                }
+            }
+        }
+    }
+    std::cout << "O numero de meteoros na agua e: " << Meteor_cont << std::endl;
+    cv::imwrite("caminho_meteor.png", image);
     // cv::imshow("image window", image);
     // cv::waitKey(0);
     return 0;
