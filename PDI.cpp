880ef3c191b7aa0ac790fac81d0cfaa232623fa4
Biel-013 <luizgomesgabrielmg@gmail.com>
2024-08-27 21:15:32 -0300

    Código finalizado, comentado e refatorado

--- PDI.cpp
+++ PDI.cpp
@@ -1,89 +1,83 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include <opencv2/core/utils/logger.hpp>
 
 int main(int argc, char **argv)
 {
-    std::string imagePATH = "meteor_challenge_01.png";
+    /* Silencia as mensagens de retorno do OpenCV*/
+    cv::utils::logging::setLogLevel(cv::utils::logging::LOG_LEVEL_SILENT);
+
+    /* Cria os contadores para armazenar o numero máximo de estrelas e
+     meteoros*/
     int Star_cont = 0;
     int Meteor_cont = 0;
+
+    /* Cria o objeto "image", que será basicamente uma matrix onde cada
+    índice apresenta um valor BGR*/
+    std::string imagePATH = "meteor_challenge_01.png";
     cv::Mat image = cv::imread(imagePATH, cv::IMREAD_COLOR);
 
+    /* Verifica se não houve erro na instanciação do objeto*/
     if (image.empty())
     {
         std::cerr << "Erro ao carregar a imagem!" << std::endl;
         return -1;
     }
-    /************************Clean image**************************/
-    cv::Vec3b pixelWhite(255, 255, 255);
-    for (int y = 0; y < image.rows; y++)
-    {
-        for (int x = 0; x < image.cols; x++)
-        {
-            if (image.at<cv::Vec3b>(y, x)[0] == pixelWhite[0] &&
-                image.at<cv::Vec3b>(y, x)[1] == pixelWhite[1] &&
-                image.at<cv::Vec3b>(y, x)[2] == pixelWhite[2])
-            {
-                image.at<cv::Vec3b>(y, x) = cv::Vec3b(189, 119, 2);
-            }
-        }
-    }
-    cv::imwrite("Clean_image.png", image);
-    /************************Clean image**************************/
-    cv::Vec3b pixelRed(0, 0, 255);
-    cv::Vec3b pixelBlue(255, 0, 0);
-    cv::Vec3b pixelYellow(0, 255, 255);
 
-    for (int y = 0; y < image.rows; y++)
-    {
-        for (int x = 0; x < image.cols; x++)
+    /* Variáveis que armazenam o valor dos pixels Vermelho, Azul e Verde */
+    cv::Vec3b pixelRed(0, 0, 255);   // BGR: Blue(0), Green(0), Red(255)
+    cv::Vec3b pixelBlue(255, 0, 0);  // BGR: Blue(255), Green(0), Red(0)
+    cv::Vec3b pixelGreen(0, 255, 0); // BGR: Blue(0), Green(255), Red(0)
+
+    /* Laço duplo for para percorrer todos os índices da imagem*/
+    for (int row = image.rows - 1; row >= 0; row--)
+        for (int col = 0; col < image.cols; col++)
         {
-            if (image.at<cv::Vec3b>(y, x)[0] == pixelRed[0] &&
-                image.at<cv::Vec3b>(y, x)[1] == pixelRed[1] &&
-                image.at<cv::Vec3b>(y, x)[2] == pixelRed[2])
+            /* Variável para armazenamento do valor do píxel*/
+            cv::Vec3b detectColor = image.at<cv::Vec3b>(row, col);
+
+            /* Essa variável armazena se o meteoro caiu ou não na água*/
+            bool inOcean = false;
+
+            /* Se um píxel vermelho for detectado, percorre um caminho
+            perpendicular até a água para verificar se o meteoro irá cair
+            no oceano ou não*/
+            if (detectColor == pixelRed)
             {
-                for (int i = y; i < image.rows; i++)
+                for (int pathway = row; pathway < image.rows; pathway++)
                 {
-                    image.at<cv::Vec3b>(y, x) = cv::Vec3b(255, 255, 255);
-                    if (image.at<cv::Vec3b>(i, x)[0] == pixelBlue[0] &&
-                        image.at<cv::Vec3b>(i, x)[1] == pixelBlue[1] &&
-                        image.at<cv::Vec3b>(i, x)[2] == pixelBlue[2])
+                    detectColor = image.at<cv::Vec3b>(pathway, col);
+
+                    if (detectColor == pixelBlue)
                     {
                         Meteor_cont++;
-                        image.at<cv::Vec3b>(y, x) = cv::Vec3b(0, 255, 255);
+                        inOcean = true;
                         break;
                     }
                 }
-            }
-        }
-    }
 
-    for (int y = 0; y < image.rows; y++)
-    {
-        for (int x = 0; x < image.cols; x++)
-        {
-            if (image.at<cv::Vec3b>(y, x)[0] == pixelWhite[0] &&
-                image.at<cv::Vec3b>(y, x)[1] == pixelWhite[1] &&
-                image.at<cv::Vec3b>(y, x)[2] == pixelWhite[2])
-            {
-                for (int i = y; i < image.rows; i++)
-                {
-                    image.at<cv::Vec3b>(i, x) = cv::Vec3b(0, 0, 255);
-                }
-            }
-            if (image.at<cv::Vec3b>(y, x)[0] == pixelYellow[0] &&
-                image.at<cv::Vec3b>(y, x)[1] == pixelYellow[1] &&
-                image.at<cv::Vec3b>(y, x)[2] == pixelYellow[2])
-            {
-                for (int i = y; i < image.rows; i++)
-                {
-                    image.at<cv::Vec3b>(i, x) = cv::Vec3b(0, 255, 0);
-                }
+                /* Caso o meteoro caia no oceano, esse novo laço irá desenhar um
+                caminho verde até o mar*/
+                if (inOcean)
+                    for (int pathway = row; pathway < image.rows; pathway++)
+                    {
+
+                        detectColor = image.at<cv::Vec3b>(pathway, col);
+
+                        if (detectColor == pixelBlue)
+                            break;
+                        image.at<cv::Vec3b>(pathway, col) = pixelGreen;
+                    }
+                /* Caso contrário, irá desenhar um caminho vermelho*/
+                else
+                    for (int pathway = row; pathway < image.rows; pathway++)
+                        image.at<cv::Vec3b>(pathway, col) = pixelRed;
             }
         }
-    }
-    std::cout << "O numero de meteoros na agua e: " << Meteor_cont << std::endl;
+
+    /* Armazena a nova imagem adquirida e exibe o número de meteoros no
+    mar no terminal*/
     cv::imwrite("caminho_meteor.png", image);
-    // cv::imshow("image window", image);
-    // cv::waitKey(0);
+    std::cout << "O numero de meteoros na agua e: " << Meteor_cont << std::endl;
     return 0;
 }
